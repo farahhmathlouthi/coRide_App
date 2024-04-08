b7d07e10a4bef2594816d541f717c8c6538d6e93
AsmaBenHamouda <asmabenhamouda08@gmail.com>
2024-04-08 02:17:22 +0100

    add animation remove status bar

--- app/src/main/java/com/example/corideapp/MainActivity.java
+++ app/src/main/java/com/example/corideapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.corideapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Variable
+    Animation topanimation, buttom;
+    ImageView image ;
+    TextView logo,slogan ;
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //to remove the status bar ..
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        //Animation
+        topanimation= AnimationUtils.loadAnimation(this,R.animator.animation);
+        buttom= AnimationUtils.loadAnimation(this,R.animator.buttom);
+        // set animation
+        image=findViewById(R.id.greenlogo);
+        image.setAnimation(buttom);
+
+
     }
 }

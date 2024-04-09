3323d53271152ca6aeab276d8abd3537855738eb
Farah_Mathlouthi <mathlouthifarah1@gmail.com>
2024-04-09 02:16:41 +0100

    adding the onclick function to the mainActivity2

--- app/src/main/java/com/example/corideapp/MainActivity2.java
+++ app/src/main/java/com/example/corideapp/MainActivity2.java
@@ -1,6 +1,11 @@
 package com.example.corideapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,20 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        final EditText inputMobile = findViewById(R.id.inputMobile);
+        Button buttonGetOTP = findViewById(R.id.continueB);
+        buttonGetOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(inputMobile.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(MainActivity2.this,"Enter Phone Number",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(getApplicationContext(),verifyOTPActivity.class);
+                intent.putExtra("mobile", inputMobile.getText().toString());
+                startActivity(intent);
+            }
         });
     }
 }

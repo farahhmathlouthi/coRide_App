70b6aae622c6c1ad707a49a0de5949eae86272c8
AsmaBenHamouda <asmabenhamouda08@gmail.com>
2024-04-09 18:04:56 +0100

    changes of gradle + animation back

--- app/src/main/java/com/example/corideapp/MainActivity.java
+++ app/src/main/java/com/example/corideapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.corideapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -11,6 +13,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN=3000;
+
     //Variable
     Animation topanimation, buttom;
     ImageView image ;
@@ -29,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
         // set animation
         image=findViewById(R.id.greenlogo);
         image.setAnimation(buttom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
 
     }
 }

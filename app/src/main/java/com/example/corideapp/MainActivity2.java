9ed710033e5691133f966b108136bccbdb7d31f0
Farah_Mathlouthi <mathlouthifarah1@gmail.com>
2024-04-23 20:15:39 +0100

    added App Logo

--- app/src/main/java/com/example/corideapp/MainActivity2.java
+++ app/src/main/java/com/example/corideapp/MainActivity2.java
@@ -1,40 +1,66 @@
 package com.example.corideapp;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.ProgressBar;
-        import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
+    public FirebaseAuth mAuth;
+    public EditText inputEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
 
-        final EditText inputMobile = findViewById(R.id.inputMobile);
+        mAuth = FirebaseAuth.getInstance();
+
+        inputEmail = findViewById(R.id.inputEmail);
         Button continueB = findViewById(R.id.continueB);
 
         final ProgressBar progressBar = findViewById(R.id.progressBar1);
         continueB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(inputMobile.getText().toString().trim().isEmpty() || inputMobile.getText().toString().trim().length() != 8 ) {
-                    Toast.makeText(MainActivity2.this,"Enter Your Phone Number",Toast.LENGTH_SHORT).show();
-                    return;
+                String email = inputEmail.getText().toString().trim();
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(MainActivity2.this,"Enter Your Email Adress !",Toast.LENGTH_SHORT).show();
+                } else {
+                    sendVerificationEmail(email);
+                    continueB.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.VISIBLE);
                 }
-                progressBar.setVisibility(View.VISIBLE);
-                continueB.setVisibility(View.INVISIBLE);
 
                 Intent intent = new Intent(getApplicationContext(),verifyOTPActivity.class);
-                intent.putExtra("mobile", inputMobile.getText().toString());
+                intent.putExtra("mobile", inputEmail.getText().toString());
                 startActivity(intent);
             }
         });
+    }
+
+    private void sendVerificationEmail(String email) {
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity2.this, "Verification email sent", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity2.this, "Failed to send verification email", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/corideapp/verifyOTPActivity.java
+++ app/src/main/java/com/example/corideapp/verifyOTPActivity.java
@@ -1,27 +1,39 @@
 package com.example.corideapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class verifyOTPActivity extends AppCompatActivity {
-
+    private ProgressBar progressBar2;
+    private Button verifyB;
     protected EditText inputCode1, inputCode2, inputCode3 , inputCode4 , inputCode5, inputCode6, inputCode7, inputCode8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otpactivity);
 
-        TextView textMobile = findViewById(R.id.textMobile);
-        textMobile.setText(String.format("+216-%s",getIntent().getStringExtra("mobile")));
+        TextView textEmail = findViewById(R.id.textEmail);
+        textEmail.setText(String.format("+216-%s",getIntent().getStringExtra("mobile")));
+        verifyB = findViewById(R.id.verifyB);
+
+        progressBar2 = findViewById(R.id.progressBar2);
+        TextView resendlabel = findViewById(R.id.textResendOTP);
 
         inputCode1 = findViewById(R.id.inputCode1);
         inputCode2 = findViewById(R.id.inputCode2);
@@ -33,16 +45,68 @@ public class verifyOTPActivity extends AppCompatActivity {
         inputCode8 = findViewById(R.id.inputCode8);
 
         setupOTPInputs();
+
+
+
+        ///resendlabel.setOnClickListener(new View.OnClickListener() {});
+        verifyB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!inputCode1.getText().toString().trim().isEmpty() && !inputCode2.getText().toString().trim().isEmpty() && !inputCode3.getText().toString().trim().isEmpty() && !inputCode4.getText().toString().trim().isEmpty() && !inputCode5.getText().toString().trim().isEmpty() && !inputCode6.getText().toString().trim().isEmpty() && !inputCode7.getText().toString().trim().isEmpty() && !inputCode8.getText().toString().trim().isEmpty()) {
+                    String numberotp = inputCode1.getText().toString() +
+                            inputCode2.getText().toString() +
+                            inputCode3.getText().toString() +
+                            inputCode4.getText().toString() +
+                            inputCode5.getText().toString() +
+                            inputCode6.getText().toString() +
+                            inputCode7.getText().toString() +
+                            inputCode8.getText().toString() ;
+
+                    //if (getotpbackend!=null){
+                    //progressBar2.setVisibility(View.VISIBLE);
+                    // verifyB.setVisibility(View.INVISIBLE);
+                    verifyOTP(numberotp);
+
+                } else{
+                    Toast.makeText(verifyOTPActivity.this, "Please Enter All Numbers", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private void verifyOTP(String numberotp) {
+        progressBar2.setVisibility(View.VISIBLE);
+
+        String email = getIntent().getStringExtra("email"); // Get the email from Intent
+
+        // Use EmailAuthProvider to sign in with email and password
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, numberotp)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar2.setVisibility(View.GONE);
+                        verifyB.setVisibility(View.VISIBLE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(verifyOTPActivity.this, "OTP verified", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(verifyOTPActivity.this, Home.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(verifyOTPActivity.this, "Incorrect OTP", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    String getotpbackend;
     private void setupOTPInputs() {
         inputCode1.addTextChangedListener(new TextWatcher() {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                 if(!s.toString().trim().isEmpty()) {
-                        inputCode2.requestFocus();
-                 }
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode2.requestFocus();
+                }
             }
             @Override
             public void afterTextChanged(Editable s) {
